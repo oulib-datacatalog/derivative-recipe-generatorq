b1c6200b36e0535fd437a737d8a680afe77b4181
Tyler Pearson <tdpearson@ou.edu>
2021-04-26 16:27:53 -0500

    tick version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='derivative_recipe_generatorq',
-      version='0.0.1',
+      version='0.1.0',
       packages= find_packages(),
       install_requires=[
           'celery',
